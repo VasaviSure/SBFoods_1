f6996613d31d98e829d070bc1914dcfe839042d7
praneetha-ch <praneethachaladi@gmail.com>
2024-07-14 23:57:34 +0530

    Second Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+//import './Nabvar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src ={assets.logo} alt=""/>
+    </div>
+  )
+}
+
+export default Navbar
